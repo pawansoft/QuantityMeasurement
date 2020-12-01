775fcd639d47a6e2930de82db91fb8a2f7c798b3
Pawan Kumar <pk.soft29@gmail.com>
2020-12-01 20:20:09 +0530

    Refactor : To update Test Suite

--- com.quantityMeasurment.test/quantityMeasurement.test.js
+++ com.quantityMeasurment.test/quantityMeasurement.test.js
@@ -152,3 +152,18 @@ const exception = require('../com.quantityMeasurment/exceptionType')
             return expect(isEqual).resolves.toEqual(true)
         })
     })
+
+    describe('Adding_LiterMeasurment_Test', () => {
+        test('Provide1Gallon3.78Liter_WhenUnitAdded_ShouldReturn7.56Liter', () => {
+            const firstValue = new Quantity(length.GALLON, 1);
+            const secondValue = new Quantity(length.LITER, 3.78);
+            const sumOfUnit = firstValue.addQuantity(secondValue, length.LITER);
+            return expect(sumOfUnit).resolves.toEqual(7.56)
+        })
+        test('Provide1Liter1000ML_WhenAdded_ShouldReturn2Liter', () => {
+            const firstValue = new Quantity(length.LITER, 1);
+            const secondValue = new Quantity(length.MILLI_LITER, 1000);
+            const sumOfUnit = firstValue.addQuantity(secondValue, length.LITER);
+            return expect(sumOfUnit).resolves.toEqual(2)
+        })
+    })
